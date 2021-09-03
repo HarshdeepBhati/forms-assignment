08554e59117d19e9add00fb6b99043715163a79e
HarshdeepBhati <hashdeep.rana.377@gmail.com>
2021-09-03 20:35:44 +0530

    final Commit

--- src/app/form-assignment/form-assignment.component.ts
+++ src/app/form-assignment/form-assignment.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators,FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-form-assignment',
   templateUrl: './form-assignment.component.html',
@@ -9,21 +9,21 @@ export class FormAssignmentComponent implements OnInit {
 
   getFullName = '';
   getEmail = '';
-  getMobileNumber='';
+  getMobileNumber = '';
   getCity = '';
-  getFamilyName:any;
+  getFamilyName: any;
   getFamilyRelation: any;
   utilityArray = [];
-  count : number = 0;
+  count: number = 0;
 
-registerForm: FormGroup
-  constructor(private fb: FormBuilder) { 
+  registerForm: FormGroup
+  constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      fullName: ['',Validators.required],
-      emailId: ['',[Validators.email,Validators.required]],
-      createPassword:['',[Validators.required,Validators.minLength(6)]],
-      mobileNumber:['',[Validators.required,Validators.pattern('[- +()0-9]+')]],
-      currentCity:['',Validators.required],
+      fullName: ['', Validators.required],
+      emailId: ['', [Validators.email, Validators.required]],
+      createPassword: ['', [Validators.required, Validators.minLength(6)]],
+      mobileNumber: ['', [Validators.required, Validators.pattern('[- +()0-9]+')]],
+      currentCity: ['', Validators.required],
       family: this.fb.array([])
     });
   }
@@ -31,27 +31,25 @@ registerForm: FormGroup
 
   ngOnInit(): void {
   }
-  
-  family():FormArray
-  {
+
+  family(): FormArray {
     return this.registerForm.get('family') as FormArray;
   }
   newFamily(): FormGroup {
     return this.fb.group({
-      rname:['',Validators.required],
-      relation:['',Validators.required],
+      rname: ['', Validators.required],
+      relation: ['', Validators.required],
     })
   }
-  addFamily(){
+  addFamily() {
     this.family().push(this.newFamily());
   }
-  onSubmit(){
-    this.getFullName = this.registerForm.get('fullName')?.value; 
+  onSubmit() {
+    this.getFullName = this.registerForm.get('fullName')?.value;
     this.getEmail = this.registerForm.get('emailId')?.value;
     this.getMobileNumber = this.registerForm.get('mobileNumber')?.value;
     this.getCity = this.registerForm.get('currentCity')?.value;
-    for(let i=0;i<=this.count;i++)
-    {
+    for (let i = 0; i <= this.count; i++) {
       this.getFamilyName = this.family().at(i);
     }
     // console.log(this.registerForm.value.family);
@@ -59,10 +57,9 @@ registerForm: FormGroup
     // this.utilityArray = Object.values(this.getFamilyName)
     // console.log(this.utilityArray);
   }
- 
-  removeFamily()
-  {
-    this.family().removeAt(this.family().length-1);
+
+  removeFamily() {
+    this.family().removeAt(this.family().length - 1);
   }
-  
+
 }
