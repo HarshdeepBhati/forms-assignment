5d24f201d41f97540e26307519949265b98318cb
HarshdeepBhati <hashdeep.rana.377@gmail.com>
2021-09-03 20:33:30 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FormAssignmentComponent } from './form-assignment/form-assignment.component';
 import { FormsModule } from '@angular/forms';
+import { DisplayComponent } from './display/display.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FormAssignmentComponent
+    FormAssignmentComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
--- src/app/form-assignment/form-assignment.component.ts
+++ src/app/form-assignment/form-assignment.component.ts
@@ -7,35 +7,62 @@ import { FormGroup, FormControl, FormBuilder, Validators,FormArray } from '@angu
 })
 export class FormAssignmentComponent implements OnInit {
 
-registerForm = this.fb.group(
-  {
-    fullName: ['',Validators.required],
-    emailId: ['',[Validators.email,Validators.required]],
-    createPassword:['',[Validators.required,Validators.minLength(6)]],
-    mobileNumber:['',[Validators.required,Validators.pattern('[- +()0-9]+')]],
-    currentCity:['',Validators.required],
-    family: this.fb.array([
-      this.fb.control('')
-    ],Validators.required)
-  }
-);
+  getFullName = '';
+  getEmail = '';
+  getMobileNumber='';
+  getCity = '';
+  getFamilyName:any;
+  getFamilyRelation: any;
+  utilityArray = [];
+  count : number = 0;
 
-  constructor(private fb: FormBuilder) { }
+registerForm: FormGroup
+  constructor(private fb: FormBuilder) { 
+    this.registerForm = this.fb.group({
+      fullName: ['',Validators.required],
+      emailId: ['',[Validators.email,Validators.required]],
+      createPassword:['',[Validators.required,Validators.minLength(6)]],
+      mobileNumber:['',[Validators.required,Validators.pattern('[- +()0-9]+')]],
+      currentCity:['',Validators.required],
+      family: this.fb.array([])
+    });
+  }
+
 
   ngOnInit(): void {
   }
-  get family()
+  
+  family():FormArray
   {
     return this.registerForm.get('family') as FormArray;
   }
-  onSubmit(){
-    console.log(this.registerForm.value)
+  newFamily(): FormGroup {
+    return this.fb.group({
+      rname:['',Validators.required],
+      relation:['',Validators.required],
+    })
   }
   addFamily(){
-    this.family.push(this.fb.control(''));
+    this.family().push(this.newFamily());
   }
+  onSubmit(){
+    this.getFullName = this.registerForm.get('fullName')?.value; 
+    this.getEmail = this.registerForm.get('emailId')?.value;
+    this.getMobileNumber = this.registerForm.get('mobileNumber')?.value;
+    this.getCity = this.registerForm.get('currentCity')?.value;
+    for(let i=0;i<=this.count;i++)
+    {
+      this.getFamilyName = this.family().at(i);
+    }
+    // console.log(this.registerForm.value.family);
+    // console.log(this.getFamilyName);
+    // this.utilityArray = Object.values(this.getFamilyName)
+    // console.log(this.utilityArray);
+  }
+ 
   removeFamily()
   {
-    this.family.removeAt(this.family.length-1);
+    this.family().removeAt(this.family().length-1);
   }
+  
 }
